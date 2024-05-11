664982a1da7de97f3c15ffd56b06b650efce39d7
Divya panchori <divya.p@iqm.com>
2024-05-11 09:58:35 +0530

    backend connected

--- frontend/src/components/newcollections/NewCollections.jsx
+++ frontend/src/components/newcollections/NewCollections.jsx
@@ -1,25 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import './NewCollections.css'
-import Item from '../item/Item'
+import React, { useEffect, useState } from 'react';
+import './NewCollections.css';
+import Item from '../item/Item';
 
 export const NewCollections = () => {
-
-  const [new_collection,setNewCollection] = useState([]);
-  useEffect(()=>{
-    fetch('http://localhost:4000/newcollections')
-    .then((response)=> response.json())
-    .then((data)=> setNewCollection(data));
-  },[])
+  const [new_collection, setNewCollection] = useState([]);
+  useEffect(() => {
+    fetch('https://jydagjysdvagkuasdu.onrender.com/newcollections')
+      .then((response) => response.json())
+      .then((data) => setNewCollection(data));
+  }, []);
   return (
     <div className='new-collections'>
-       <h1>NEW COLLECTIONS</h1> 
-       <hr />
-       <div className="collections">
-        {new_collection.map((item,i)=> {
-            return<Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-        }
-        )}
-       </div>
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className='collections'>
+        {new_collection.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
--- frontend/src/components/popular/Popular.jsx
+++ frontend/src/components/popular/Popular.jsx
@@ -1,26 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import './Popular.css'
-import Item from '../item/Item'
+import React, { useEffect, useState } from 'react';
+import './Popular.css';
+import Item from '../item/Item';
 
 export const Popular = () => {
-  const [popular_Products, setPopular_Products]= useState([]);
+  const [popular_Products, setPopular_Products] = useState([]);
 
-  useEffect(()=>{
-    fetch('http://localhost:4000/popularinwomen')
-    .then((response)=>response.json())
-    .then((data)=>setPopular_Products(data));
-    console.log("popular fetched");
-  },[])
-  
+  useEffect(() => {
+    fetch('https://jydagjysdvagkuasdu.onrender.com/popularinwomen')
+      .then((response) => response.json())
+      .then((data) => setPopular_Products(data));
+    console.log('popular fetched');
+  }, []);
+
   return (
     <div className='popular'>
-        <h1>POPULAR IN WOMEN</h1>
-        <hr />
-        <div className="popular-item">
-            {popular_Products.map((item,i)=>{
-                return<Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-            })}
-        </div>
+      <h1>POPULAR IN WOMEN</h1>
+      <hr />
+      <div className='popular-item'>
+        {console.log(popular_Products)}
+        {popular_Products.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
